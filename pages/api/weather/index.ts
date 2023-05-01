67108f305c7e9981887caebea6b595543ce99a43
Phanichander K <phanichander.katakam@gmail.com>
2023-05-01 23:32:19 +0800

    code cleanup

--- pages/api/weather/index.ts
+++ pages/api/weather/index.ts
@@ -1,26 +1,5 @@
-import { DATA_GOV_SG_API } from '@/constants';
 import axios from 'axios';
 import type { NextApiRequest, NextApiResponse } from 'next'
-
-type Data = {
-  name: string
-}
-
-export const comments = [
-  {
-    id: 1,
-    text: 'This is the first comment'
-  },
-  {
-    id: 2,
-    text: 'This is the second comment'
-  },
-  {
-    id: 3,
-    text: 'This is the third comment'
-  }
-]
-
 
 export default async function handler(
   req: NextApiRequest,
@@ -34,7 +13,7 @@ export default async function handler(
      return response;
     }).catch(error => {
       console.error(error)
-      // reject(error.response);
+      return error;
     });
   })
 
